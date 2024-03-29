13c49d3e3b85c0cbdc9d8de358b6487fed4202a8
telcobot <147593099+telcobot@users.noreply.github.com>
2024-03-29 05:47:02 -0300

    chore: Update charm libraries (#271)

--- lib/charms/tls_certificates_interface/v3/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v3/tls_certificates.py
@@ -19,7 +19,7 @@ charmcraft fetch-lib charms.tls_certificates_interface.v3.tls_certificates
 
 Add the following libraries to the charm's `requirements.txt` file:
 - jsonschema
-- cryptography
+- cryptography >= 42.0.0
 
 Add the following section to the charm's `charmcraft.yaml` file:
 ```yaml
@@ -316,7 +316,7 @@ LIBAPI = 3
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 8
+LIBPATCH = 9
 
 PYDEPS = ["cryptography", "jsonschema"]
 
