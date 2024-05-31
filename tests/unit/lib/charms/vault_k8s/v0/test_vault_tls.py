1707ec049ebc0f7d69aff62feb413e22bed66bf4
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-05-31 08:38:31 -0400

    chore: use correct dependencies

--- tests/unit/lib/charms/vault_k8s/v0/test_vault_tls.py
+++ tests/unit/lib/charms/vault_k8s/v0/test_vault_tls.py
@@ -200,7 +200,7 @@ class TestCharmTLS(unittest.TestCase):
         self.harness.set_can_connect(container=self.container_name, val=True)
 
         self.harness.charm.on.config_changed.emit()
-        assert patch_restart.called_once()
+        patch_restart.assert_called_once()
         self.assertEqual((root / "vault/certs/ca.pem").read_text(), EXAMPLE_CA)
 
     @patch("charms.vault_k8s.v0.vault_client.Vault.enable_audit_device", new=Mock)
