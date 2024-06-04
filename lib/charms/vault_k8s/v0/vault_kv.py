4e45313cedf5da4723352638632494cc3016b245
yazansalti <yazan.salti@canonical.com>
2024-06-04 17:00:28 +0400

    Bumps libpatch

--- lib/charms/vault_k8s/v0/vault_kv.py
+++ lib/charms/vault_k8s/v0/vault_kv.py
@@ -132,7 +132,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 6
+LIBPATCH = 7
 
 PYDEPS = ["pydantic", "pytest-interface-tester"]
 
