469eda853dfa814d3104bf7e5bbc240ecf4aa015
guillaume <guillaume.belanger27@gmail.com>
2024-08-26 08:47:00 -0400

    chore: add unit tests for vault PKI
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/unit/fixtures.py
+++ tests/unit/fixtures.py
@@ -6,13 +6,13 @@ from unittest.mock import patch
 import pytest
 import scenario
 from charms.data_platform_libs.v0.s3 import S3Requirer
-from charms.vault_k8s.v0.vault_client import (
-    Vault,
-)
+from charms.vault_k8s.v0.vault_client import Vault
 from charms.vault_k8s.v0.vault_s3 import S3
 from charms.vault_k8s.v0.vault_tls import VaultTLSManager
 
 from charm import VaultCharm
+
+TLS_CERTIFICATES_LIB_PATH = "charms.tls_certificates_interface.v3.tls_certificates"
 
 
 class VaultCharmFixtures:
@@ -21,6 +21,18 @@ class VaultCharmFixtures:
     patcher_s3_requirer = patch("charm.S3Requirer", autospec=S3Requirer)
     patcher_s3 = patch("charm.S3", autospec=S3)
     patcher_socket_fqdn = patch("socket.getfqdn")
+    patcher_pki_requirer_request_certificate_creation = patch(
+        f"{TLS_CERTIFICATES_LIB_PATH}.TLSCertificatesRequiresV3.request_certificate_creation"
+    )
+    patcher_pki_requirer_get_assigned_certificates = patch(
+        f"{TLS_CERTIFICATES_LIB_PATH}.TLSCertificatesRequiresV3.get_assigned_certificates"
+    )
+    patcher_pki_provider_get_outstanding_certificate_requests = patch(
+        f"{TLS_CERTIFICATES_LIB_PATH}.TLSCertificatesProvidesV3.get_outstanding_certificate_requests"
+    )
+    patcher_pki_provider_set_relation_certificate = patch(
+        f"{TLS_CERTIFICATES_LIB_PATH}.TLSCertificatesProvidesV3.set_relation_certificate"
+    )
 
     @pytest.fixture(autouse=True)
     def setup(self):
@@ -29,6 +41,18 @@ class VaultCharmFixtures:
         self.mock_s3_requirer = VaultCharmFixtures.patcher_s3_requirer.start().return_value
         self.mock_s3 = VaultCharmFixtures.patcher_s3.start()
         self.mock_socket_fqdn = VaultCharmFixtures.patcher_socket_fqdn.start()
+        self.mock_pki_requirer_request_certificate_creation = (
+            VaultCharmFixtures.patcher_pki_requirer_request_certificate_creation.start()
+        )
+        self.mock_pki_requirer_get_assigned_certificates = (
+            VaultCharmFixtures.patcher_pki_requirer_get_assigned_certificates.start()
+        )
+        self.mock_pki_provider_get_outstanding_certificate_requests = (
+            VaultCharmFixtures.patcher_pki_provider_get_outstanding_certificate_requests.start()
+        )
+        self.mock_pki_provider_set_relation_certificate = (
+            VaultCharmFixtures.patcher_pki_provider_set_relation_certificate.start()
+        )
 
     @pytest.fixture(autouse=True)
     def context(self):
--- tests/unit/test_charm_configure.py
+++ tests/unit/test_charm_configure.py
@@ -7,7 +7,16 @@ import tempfile
 
 import hcl
 import scenario
+from charms.vault_k8s.v0.vault_client import (
+    Certificate,
+    SecretsBackend,
+)
+from ops.pebble import Layer
 
+from tests.unit.certificates import (
+    generate_example_provider_certificate,
+    generate_example_requirer_csr,
+)
 from tests.unit.fixtures import VaultCharmFixtures
 
 
@@ -52,3 +61,256 @@ class TestCharmConfigure(VaultCharmFixtures):
             actual_content_hcl = hcl.loads(actual_config)
             expected_content_hcl = hcl.loads(expected_config)
             assert actual_content_hcl == expected_content_hcl
+
+    def test_given_leader_when_configure_then_pebble_layer_is_planned(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            vault_config_mount = scenario.Mount(
+                location="/vault/config",
+                src=temp_dir,
+            )
+            container = scenario.Container(
+                name="vault",
+                can_connect=True,
+                mounts={
+                    "vault-config": vault_config_mount,
+                },
+            )
+            peer_relation = scenario.PeerRelation(
+                endpoint="vault-peers",
+            )
+            state_in = scenario.State(
+                containers=[container],
+                leader=True,
+                relations=[peer_relation],
+            )
+
+            state_out = self.ctx.run(container.pebble_ready_event, state_in)
+
+            assert state_out.containers[0].layers == {
+                "vault": Layer(
+                    {
+                        "summary": "vault layer",
+                        "description": "pebble config layer for vault",
+                        "services": {
+                            "vault": {
+                                "summary": "vault",
+                                "startup": "enabled",
+                                "override": "replace",
+                                "command": "vault server -config=/vault/config/vault.hcl",
+                            }
+                        },
+                    }
+                )
+            }
+
+    # PKI
+
+    def test_given_vault_active_when_configure_then_pki_secrets_engine_is_configured(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_vault.configure_mock(
+                **{
+                    "is_api_available.return_value": True,
+                    "authenticate.return_value": True,
+                    "is_initialized.return_value": True,
+                    "is_sealed.return_value": False,
+                    "is_active_or_standby.return_value": True,
+                    "generate_pki_intermediate_ca_csr.return_value": "my csr",
+                },
+            )
+            self.mock_pki_requirer_get_assigned_certificates.return_value = []
+            vault_config_mount = scenario.Mount(
+                location="/vault/config",
+                src=temp_dir,
+            )
+            container = scenario.Container(
+                name="vault",
+                can_connect=True,
+                mounts={
+                    "vault-config": vault_config_mount,
+                },
+            )
+            peer_relation = scenario.PeerRelation(
+                endpoint="vault-peers",
+            )
+            pki_relation = scenario.Relation(
+                endpoint="tls-certificates-pki",
+                interface="tls-certificates",
+            )
+            approle_secret = scenario.Secret(
+                id="0",
+                label="vault-approle-auth-details",
+                contents={0: {"role-id": "role id", "secret-id": "secret id"}},
+            )
+            state_in = scenario.State(
+                containers=[container],
+                leader=True,
+                secrets=[approle_secret],
+                relations=[peer_relation, pki_relation],
+                config={"common_name": "myhostname.com"},
+            )
+
+            self.ctx.run(container.pebble_ready_event, state_in)
+
+            self.mock_vault.enable_secrets_engine.assert_called_once_with(
+                SecretsBackend.PKI, "charm-pki"
+            )
+            self.mock_vault.generate_pki_intermediate_ca_csr.assert_called_once_with(
+                mount="charm-pki",
+                common_name="myhostname.com",
+            )
+            self.mock_pki_requirer_request_certificate_creation.assert_called_once_with(
+                certificate_signing_request="my csr".encode(),
+                is_ca=True,
+            )
+
+    def test_given_vault_is_available_when_pki_certificate_is_available_then_certificate_added_to_vault_pki_and_latest_issuer_set_to_default(
+        self,
+    ):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_vault.configure_mock(
+                **{
+                    "is_api_available.return_value": True,
+                    "authenticate.return_value": True,
+                    "is_initialized.return_value": True,
+                    "is_sealed.return_value": False,
+                    "is_active_or_standby.return_value": True,
+                    "generate_pki_intermediate_ca_csr.return_value": "my csr",
+                    "is_common_name_allowed_in_pki_role.return_value": False,
+                },
+            )
+            vault_config_mount = scenario.Mount(
+                location="/vault/config",
+                src=temp_dir,
+            )
+            container = scenario.Container(
+                name="vault",
+                can_connect=True,
+                mounts={
+                    "vault-config": vault_config_mount,
+                },
+            )
+            peer_relation = scenario.PeerRelation(
+                endpoint="vault-peers",
+            )
+            pki_relation = scenario.Relation(
+                endpoint="tls-certificates-pki",
+                interface="tls-certificates",
+            )
+            provider_certificate = generate_example_provider_certificate(
+                common_name="myhostname.com",
+                relation_id=pki_relation.relation_id,
+            )
+            self.mock_pki_requirer_get_assigned_certificates.return_value = [provider_certificate]
+            approle_secret = scenario.Secret(
+                id="0",
+                label="vault-approle-auth-details",
+                contents={0: {"role-id": "role id", "secret-id": "secret id"}},
+            )
+            state_in = scenario.State(
+                containers=[container],
+                leader=True,
+                secrets=[approle_secret],
+                relations=[peer_relation, pki_relation],
+                config={"common_name": "myhostname.com"},
+            )
+
+            self.ctx.run(container.pebble_ready_event, state_in)
+
+            self.mock_vault.set_pki_intermediate_ca_certificate.assert_called_once_with(
+                certificate=provider_certificate.certificate,
+                mount="charm-pki",
+            )
+            self.mock_vault.make_latest_pki_issuer_default.assert_called_once_with(
+                mount="charm-pki",
+            )
+            self.mock_vault.create_or_update_pki_charm_role.assert_called_once_with(
+                allowed_domains="myhostname.com",
+                mount="charm-pki",
+                role="charm",
+            )
+
+    def test_given_vault_available_when_vault_pki_certificate_creation_request_then_certificate_is_provided(
+        self,
+    ):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_vault.configure_mock(
+                **{
+                    "is_api_available.return_value": True,
+                    "authenticate.return_value": True,
+                    "is_initialized.return_value": True,
+                    "is_sealed.return_value": False,
+                    "is_active_or_standby.return_value": True,
+                    "generate_pki_intermediate_ca_csr.return_value": "my csr",
+                    "is_common_name_allowed_in_pki_role.return_value": False,
+                    "sign_pki_certificate_signing_request.return_value": Certificate(
+                        certificate="my certificate",
+                        ca="my ca",
+                        chain=["my ca"],
+                    ),
+                },
+            )
+            vault_config_mount = scenario.Mount(
+                location="/vault/config",
+                src=temp_dir,
+            )
+            container = scenario.Container(
+                name="vault",
+                can_connect=True,
+                mounts={
+                    "vault-config": vault_config_mount,
+                },
+            )
+            peer_relation = scenario.PeerRelation(
+                endpoint="vault-peers",
+            )
+            pki_relation_provider = scenario.Relation(
+                endpoint="tls-certificates-pki",
+                interface="tls-certificates",
+                remote_app_name="tls-provider",
+            )
+            pki_relation_requirer = scenario.Relation(
+                endpoint="vault-pki",
+                interface="tls-certificates",
+                remote_app_name="tls-requirer",
+            )
+            provider_certificate = generate_example_provider_certificate(
+                common_name="myhostname.com",
+                relation_id=pki_relation_provider.relation_id,
+            )
+            requirer_csr = generate_example_requirer_csr(
+                common_name="subdomain.myhostname.com",
+                relation_id=pki_relation_requirer.relation_id,
+            )
+            self.mock_pki_requirer_get_assigned_certificates.return_value = [provider_certificate]
+
+            self.mock_pki_provider_get_outstanding_certificate_requests.return_value = [
+                requirer_csr
+            ]
+            approle_secret = scenario.Secret(
+                id="0",
+                label="vault-approle-auth-details",
+                contents={0: {"role-id": "role id", "secret-id": "secret id"}},
+            )
+            state_in = scenario.State(
+                containers=[container],
+                leader=True,
+                secrets=[approle_secret],
+                relations=[peer_relation, pki_relation_provider, pki_relation_requirer],
+                config={"common_name": "myhostname.com"},
+            )
+
+            self.ctx.run(container.pebble_ready_event, state_in)
+
+            self.mock_vault.sign_pki_certificate_signing_request.assert_called_once_with(
+                mount="charm-pki",
+                role="charm",
+                csr=requirer_csr.csr,
+                common_name="subdomain.myhostname.com",
+            )
+            self.mock_pki_provider_set_relation_certificate.assert_called_once_with(
+                relation_id=pki_relation_requirer.relation_id,
+                certificate="my certificate",
+                ca="my ca",
+                certificate_signing_request=requirer_csr.csr,
+                chain=["my ca"],
+            )
