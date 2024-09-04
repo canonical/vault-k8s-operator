a502e773d84df82bfcc00e0a755427c3ed0c60c9
guillaume <guillaume.belanger27@gmail.com>
2024-09-04 09:55:23 -0400

    chore: bump vault client lib version
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- lib/charms/vault_k8s/v0/vault_client.py
+++ lib/charms/vault_k8s/v0/vault_client.py
@@ -26,7 +26,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 16
+LIBPATCH = 17
 
 
 RAFT_STATE_ENDPOINT = "v1/sys/storage/raft/autopilot/state"
