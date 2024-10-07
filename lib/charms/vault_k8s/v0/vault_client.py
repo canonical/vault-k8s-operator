a5cfc0b1ae751237ff14b9abb45efc59e7bac7be
yazansalti <yazan.salti@canonical.com>
2024-10-07 13:02:03 +0400

    Use correct libpatch

--- lib/charms/vault_k8s/v0/vault_client.py
+++ lib/charms/vault_k8s/v0/vault_client.py
@@ -28,7 +28,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 20
+LIBPATCH = 19
 
 
 RAFT_STATE_ENDPOINT = "v1/sys/storage/raft/autopilot/state"
