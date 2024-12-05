e8000b4e2b14ac774b28affbdfcb4366b41ff184
Daniel Arndt <daniel.arndt@canonical.com>
2024-12-05 08:26:55 -0400

    Bump vault_autounseal lib version

--- lib/charms/vault_k8s/v0/vault_autounseal.py
+++ lib/charms/vault_k8s/v0/vault_autounseal.py
@@ -99,7 +99,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 5
+LIBPATCH = 6
 
 
 AUTOUNSEAL_CREDENTIALS_SECRET_LABEL_PREFIX = "vault-autounseal-credentials-"
