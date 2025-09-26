acf22f00ea8100127f765248bdde1e64e8cbdacb
yazansalti <yazan.salti@canonical.com>
2025-09-26 11:25:13 +0400

    Add upgrade tests for 1.18

---
+++ k8s/tests/integration/test_upgrade_first.py
@@ -0,0 +1,76 @@
+import logging
+from pathlib import Path
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+from config import (
+    APPLICATION_NAME,
+    JUJU_FAST_INTERVAL,
+    NUM_VAULT_UNITS,
+)
+from helpers import (
+    deploy_vault,
+    get_ca_cert_file_location,
+    initialize_unseal_authorize_vault,
+    refresh_application,
+    unseal_all_vault_units,
+)
+
+logger = logging.getLogger(__name__)
+
+CURRENT_TRACK_LATEST_STABLE_CHANNEL = "1.18/stable"
+CURRENT_TRACK_FIRST_STABLE_REVISION = 446
+
+
+@pytest.mark.abort_on_fail
+async def test_given_first_stable_revision_in_track_when_refresh_then_status_is_active(
+    ops_test: OpsTest, vault_charm_path: Path
+):
+    assert ops_test.model
+    logger.info("Deploying vault from Charmhub")
+    await deploy_vault(
+        ops_test=ops_test,
+        num_units=NUM_VAULT_UNITS,
+        channel=CURRENT_TRACK_LATEST_STABLE_CHANNEL,
+        revision=CURRENT_TRACK_FIRST_STABLE_REVISION,
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[APPLICATION_NAME],
+        status="blocked",
+        wait_for_exact_units=NUM_VAULT_UNITS,
+        timeout=1000,
+    )
+    root_token, unseal_key = await initialize_unseal_authorize_vault(ops_test, APPLICATION_NAME)
+
+    await ops_test.model.wait_for_idle(
+        apps=[APPLICATION_NAME],
+        status="active",
+        wait_for_exact_units=NUM_VAULT_UNITS,
+        timeout=1000,
+    )
+
+    async with ops_test.fast_forward(fast_interval=JUJU_FAST_INTERVAL):
+        logger.info("Refreshing vault from built charm")
+        await refresh_application(ops_test, APPLICATION_NAME, vault_charm_path)
+
+    logger.info("Waiting for vault to be blocked after refresh")
+    await ops_test.model.wait_for_idle(
+        apps=[APPLICATION_NAME],
+        status="blocked",
+        wait_for_exact_units=NUM_VAULT_UNITS,
+        timeout=1000,
+    )
+
+    async with ops_test.fast_forward(fast_interval=JUJU_FAST_INTERVAL):
+        await unseal_all_vault_units(
+            ops_test, unseal_key, root_token, await get_ca_cert_file_location(ops_test)
+        )
+
+    logger.info("Waiting for vault to be active after refresh")
+    await ops_test.model.wait_for_idle(
+        apps=[APPLICATION_NAME],
+        status="active",
+        wait_for_exact_units=NUM_VAULT_UNITS,
+        timeout=1000,
+    )
